d3c0ac7aafbe36fe862f86edcebcd1dce2e9dc90
Kate Sowles <katesowles@gmail.com>
2016-04-19 16:42:33 -0700

    stuck on the scope issue but running out of time. just have to move on

--- scripts/article.js
+++ scripts/article.js
@@ -1,91 +1,86 @@
-// TODO: Wrap the entire contents of this file in an IIFE.
-// Pass in to the IIFE a module, upon which objects can be attached for later access.
-function Article (opts) {
-  this.author = opts.author;
-  this.authorUrl = opts.authorUrl;
-  this.title = opts.title;
-  this.category = opts.category;
-  this.body = opts.body;
-  this.publishedOn = opts.publishedOn;
-}
+(function(module) {
 
-Article.all = [];
-
-Article.prototype.toHtml = function() {
-  var template = Handlebars.compile($('#article-template').text());
-
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  this.body = marked(this.body);
-
-  return template(this);
-};
-
-Article.loadAll = function(rawData) {
-  rawData.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  });
-
-  // DONE: Refactor this forEach code, by using a `.map` call instead, since what we are
-  // trying to accomplish is the transformation of one colleciton into another.
-
-  // rawData.forEach(function(ele) {
-  //   Article.all.push(new Article(ele));
-  // })
-  Article.all = rawData.map(function(ele) {
-    return new Article(ele);
-  });
-};
-
-// This function will retrieve the data from either a local or remote source,
-// and process it, then hand off control to the View.
-
-// TODO: Refactor this function, and provide it with a parameter of a callback function
-//(for now just a placeholder, but to be referenced at call time as a view function)
-// to execute once the loading of articles is done. We do this because we might want
-// to call other view functions, and not just the initIndexPage() that we are replacing.
-// Now, instead of calling articleView.initIndexPage(), we can simply run our callback.
-Article.fetchAll = function() {
-  if (localStorage.rawData) {
-    Article.loadAll(JSON.parse(localStorage.rawData));
-    articleView.initIndexPage();
-  } else {
-    $.getJSON('/data/hackerIpsum.json', function(rawData) {
-      Article.loadAll(rawData);
-      localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
-      articleView.initIndexPage();
-    });
+  function Article (opts) {
+    this.author = opts.author;
+    this.authorUrl = opts.authorUrl;
+    this.title = opts.title;
+    this.category = opts.category;
+    this.body = opts.body;
+    this.publishedOn = opts.publishedOn;
   }
-};
 
-// TODO: Chain together a `map` and a `reduce` call to get a rough count of all words in all articles.
-Article.numWordsAll = function() {
-  return Article.all.map(function(article) {
-    return whatShouldIReturn; // Grab the words from the `article` `body`.
-  })
-  .reduce(function(a, b) {
-    return whatShouldIReturn;// Sum up all the values!
-  });
-};
+  Article.all = [];
 
-// TODO: Chain together a `map` and a `reduce` call to produce an array of unique author names.
-Article.allAuthors = function() {
-  // Read docs on .map and .reduce! You can reference the previous
-  // `map` in the numWordsAll method to get started here.
+  Article.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#article-template').text());
 
-  // For our `reduce` -- since we are trying to return an array, we'll need to specify an accumulator type...
-  // what data type should this accumulator be and where is it placed?
-  return whatShouldIReturn;
-};
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = marked(this.body);
 
-Article.numWordsByAuthor = function() {
-  // TODO: Transform each author string into an object with 2 properties: One for
-  // the author's name, and one for the total number of words across the matching articles
-  // written by the specified author.
-  return Article.allAuthors().map(function(author) {
-    return {
-      // name:
-      // numWords: someCollection.someArrayMethod().map(...).reduce(...), ...
-    };
-  });
-};
+    return template(this);
+  };
+
+  Article.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    Article.all = rawData.map(function(ele) {
+      return new Article(ele);
+    });
+  };
+
+  Article.fetchAll = function(parsed) {
+    if (localStorage.rawData) {
+      Article.loadAll(JSON.parse(localStorage.rawData));
+      return parsed();
+    }
+    else {
+      $.getJSON('/data/hackerIpsum.json', function(rawData) {
+        Article.loadAll(rawData);
+        localStorage.rawData = JSON.stringify(rawData); // Cache the json, so we don't need to request it next time.
+        return parsed();
+      })
+  }
+
+  Article.numWordsAll = function() {
+    return Article.all.map(function(article) {
+      return article.body.split(' ').length; // Grab the words from the `article` `body`; split on the spaces to grab each word
+    })
+    .reduce(function(a, b) {
+      return a + b;// Sum up all the values!
+    });
+  };
+
+  Article.allAuthors = function() {
+    var individualAuthors = Article.all.map(function(currentArticle) {
+      return currentArticle.author;
+    })
+    .reduce(function(previous, next) {
+      if (previous.indexOf(next) < 0) {
+        previous.push(next);
+      }
+      return previous;
+    })
+    return individualAuthors;
+  };
+
+  Article.numWordsByAuthor = function() {
+
+    // DONE: Transform each author string into an object with 2 properties: One for the author's name, and one for the total number of words across the matching articles written by the specified author.
+
+    return Article.allAuthors().map(function(author) {
+      var articleObject = {};
+      articleObject.author = author;
+      articleObject.numwords = 0;
+      Article.all.forEach(function(thisArticle) {
+        if (thisArticle.author == author) {
+          articleObject.numWords += thisArticle.body.split(' ').length;
+        }
+      });
+      return articleObject;
+      })
+  };
+
+})(window);
